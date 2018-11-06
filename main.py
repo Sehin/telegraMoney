813c1028e0eb3aa6392dbf5eb12e9291e3abad19
Sehin <toribash.ds@gmail.com>
2018-11-06 07:40:08 +0300

    Create first classes files

---
+++ main.py
@@ -0,0 +1,10 @@
+from classes.DBWorker import DBWorker
+from classes.TelegramBot import TelegramBot
+
+def main():
+    dbWorker = DBWorker()
+    bot = TelegramBot()
+    pass
+
+if __name__=='__main__':
+    main()
